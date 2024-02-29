c2f0fc049b71b2a68c157de71e2f1d340b45d254
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-02-29 08:39:08 +0000

    Add item key and a for loop

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def get_todo_list(request):
-    return render(request, 'todo/todo_list.html')
+    items = Item.objects.all()
+    context = {
+        'items': Items
+    }
+    return render(request, 'todo/todo_list.html', context)
  

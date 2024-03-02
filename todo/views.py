a2a64b6b80513573780cf26833fa346f6f7b5a16
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-02 18:15:29 +0000

    Edit item and  Delete item

--- todo/views.py
+++ todo/views.py
@@ -25,3 +25,22 @@ def add_item(request):
     }
     return render(request, 'todo/add_item.html', context)
 
+def edit_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    if request.method == 'POST':
+        form  = ItemForm(request.POST, instance=item)
+        if form.is_valid():
+            form.save()
+            return redirect('get_todo_list')
+    form = ItemForm(instance=item)
+    context = {
+        'form': form
+    }
+    return render(requset, "todo/edit_item.html")
+
+def toggle_item(request, item_id):
+    item = get_object_or_404(Item, id=item_id)
+    item.done = not item.done
+    item.save()
+    return redirect('get_todo_list')
+

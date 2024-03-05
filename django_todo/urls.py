88d212e6188e06ff4f92257c692aed77a6de9d1a
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-05 12:14:53 +0000

    Made changes in the in url.py and to todo_lits.html

--- django_todo/urls.py
+++ django_todo/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from todo import views get_todo_list
+from todo.views import get_todo_list, add_item
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),

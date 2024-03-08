dc2572ad8d257f2cc6bac8465aadec1cd6b2c4c0
Graciekan21 Nemusesu <graciekan21@gmail.com>
2024-03-08 19:25:31 +0000

    All changes

--- todo/test_forms.py
+++ todo/test_forms.py
@@ -4,7 +4,7 @@ from .forms import ItemForm
 class TestItemForm(TestCase):
 
     def test_item_name_is_required(self):
-          form = ItemForm({'name': ''})
+        form = ItemForm({'name': ''})
         self.assertFalse(form.is_valid())
         self.assertIn('name', form.errors.keys())
         self.assertEqual(form.errors['name'][0], 'This field is required.')
--- todo/test_views.py
+++ todo/test_views.py
@@ -1,11 +1,44 @@
 from django.test import TestCase
-
+from .forms import ItemForm
 
 # Create your tests here.
-class TestDjango(TestCase):
+class TestViews(TestCase):
 
-    def test_this_thing_works1(self):
-        self.assertEqual(1, 1)
+    def test_get_todo_list(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/todo_list.html')
+
+    def test_get_add_item_page(self):
+        response = self.client.get('/add')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'todo/add_item.html')
+
+    def test_can_edit_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.post(f'/edit/{item.id}', {'name': 'Updated Name'})
+        self.assertRedirects(response, '/')
+        updated_item = Item.objects.get(id=item.id)
+        self.assertEqual(updated_item.name, 'Updated Name')
+        
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/delete/{item.id}')
+        self.assertRedirects(response, '/')
+        existing_items = Item.objects.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
+
+    def test_can_delete_item(self):
+        item = Item.objects.create(name='Test Todo Item')
+        response = self.client.get(f'/delete/{item.id}')
+        self.assertRedirects(response, '/')
+        existing_items = Item.objects.filter(id=item.id)
+        self.assertEqual(len(existing_items), 0)
+
+
+
+
+
 
    
 
